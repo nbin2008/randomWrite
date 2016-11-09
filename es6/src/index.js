8507f1755e933858406196d435c4e4f60ee26f55
nbin <liubin2@mgtv.com>
2016-11-09 23:31:26 +0800

    bk

--- es6/src/index.js
+++ es6/src/index.js
@@ -1,7 +1,5 @@
-require("babel-polyfill");
-let test = "es6 is ok";
-let test2 = [1,2].includes(1);
-console.log( test );
-console.log( test2 );
-
+let data = [{a:1},{a:2},{a:3}];
+for(let {a} of data){
+	console.log(a);
+}
 

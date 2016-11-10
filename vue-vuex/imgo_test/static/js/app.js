fc47f1a1e0735631d52ff2d90b846825ff961b0e
刘斌 <liubin2@mgtv.com>
2016-11-10 18:34:05 +0800

    bk

---
+++ vue-vuex/imgo_test/static/js/app.js
@@ -0,0 +1,15 @@
+
+import Vue from "vue";
+//import Vuex from "vuex";
+//import babel_polyfill from "babel-polyfill";
+
+//Vue.use(Vuex);
+
+let vm = new Vue({
+	el: '#app',
+	//render : h=>h(),
+	data: {
+		tro: "测试测试"
+	}
+})
+
---
+++ vue-vuex/imgo_test/webpack.config.js
@@ -0,0 +1,27 @@
+
+var webpack = require('webpack');
+
+module.exports = {
+	devtool: 'source-map',
+	
+	entry: __dirname + "/static/js/app.js",
+	
+	output: {
+		path: __dirname + "/build",
+		filename: "main.js",
+	},
+
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				loader: 'babel',
+				exclude: /node_modules/,
+				query: {
+					presets: ['latest']
+				}
+			},
+		]
+	}
+}
+

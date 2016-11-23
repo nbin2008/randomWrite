ee368c812a6a3fefc05239cbfe129579bab04b5a
刘斌 <liubin2@mgtv.com>
2016-11-23 17:03:15 +0800

    bk

---
+++ vue-router/t2/static/app.js
@@ -0,0 +1,41 @@
+
+import Vue from "vue";
+import VueRouter from "vue-router";
+import App from "./App.vue";
+
+const User = {
+  template: `
+    <div class="user">
+      <h2>User {{ $route.params.id }}</h2>
+      <router-view></router-view>
+    </div>
+  `
+}
+
+const UserHome = { template: '<div>Home</div>' }
+const UserProfile = { template: '<div>Profile</div>' }
+const UserPosts = { template: '<div>Posts</div>' }
+
+const router = new VueRouter({
+	routes: [
+		{
+			path: '/user/:id',
+			component: User,
+			children: [
+				{
+					path: '',
+					component: UserHome,
+				},
+				{
+					path: 'profile',
+					component: UserProfile,
+				},
+				{
+					path: 'posts',
+					component: UserPosts,
+				}
+			]
+		}
+	]
+})
+
